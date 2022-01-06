9ceed4c48a76f0f491c2dda44367e28032a5b144
TausifM <nisargcontact2020@gmail.com>
2022-01-06 09:41:37 +0530

    localStorage, useEffect isValid

--- src/component/Subscription App/FormSubscription.jsx
+++ src/component/Subscription App/FormSubscription.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const FormSubscription = (props) => {
   // const [title, setTitle] = useState();
   // If you want to use the state in the component, you need to use the hook useState,
@@ -9,7 +9,11 @@ const FormSubscription = (props) => {
     userAmount: "",
     userDate: "",
   });
+  const [isValid, setValid] = useState(true);
   const onTitleChangeHandler = (events) => {
+    // if (events.target.value.trim().length > 0) {
+    //   setValid(true);
+    // }
     setForm((prevState) => {
       return { ...prevState, userTitle: events.target.value };
     });
@@ -29,16 +33,36 @@ const FormSubscription = (props) => {
   };
   const onSubmitHandler = (events) => {
     events.preventDefault();
+    if (form.userTitle.trim().length === 0) {
+      setValid(false);
+      return;
+    }
     const Subscription = {
       title: form.userTitle,
       amount: form.userAmount,
       date: new Date(form.userDate),
     };
     props.onSave(Subscription);
+
     // by onsave as a props we can pass data to parent component
     // this subscription send data of input as a props to new subscription file
     console.log("on submit", Subscription);
   };
+  useEffect(() => {
+    if (form.userTitle.trim().length > 0) {
+      setValid(true);
+    }
+    const timerId = setTimeout(() => {
+      if (form.userTitle.trim().length > 0) {
+        setValid(true);
+        console.log("using effect");
+      }
+    }, 3000);
+    return () => {
+      clearTimeout(timerId);
+      console.log("clean up");
+    };
+  }, [form.userTitle]);
   return (
     <form className="form" onSubmit={onSubmitHandler}>
       <div>
@@ -47,6 +71,8 @@ const FormSubscription = (props) => {
           type="text"
           onChange={onTitleChangeHandler}
           value={form.userTitle}
+          // style={{ color: !isValid ? "red" : "green" }}
+          style={{ borderColor: !isValid ? "red" : "green" }}
         ></input>
       </div>
       <div>
@@ -67,12 +93,14 @@ const FormSubscription = (props) => {
       </div>
 
       <div>
-        <button type="submit" onClick={props.onCancel}>
+        <button type="submit" className="btn danger" onClick={props.onCancel}>
           Cancel
         </button>
       </div>
       <div>
-        <button type="submit">Save</button>
+        <button type="submit" className="btn primary">
+          Save
+        </button>
       </div>
     </form>
   );
