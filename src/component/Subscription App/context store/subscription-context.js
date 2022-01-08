b521e47f8dd413ca87c16747663e77fc888796bb
TausifM <nisargcontact2020@gmail.com>
2022-01-08 13:05:20 +0530

    limitation of context

--- src/component/Subscription App/context store/subscription-context.js
+++ src/component/Subscription App/context store/subscription-context.js
@@ -3,6 +3,20 @@ import React from "react";
 const SubscriptionContext = React.createContext({
   subscriptionsList: [],
 });
+// custom context provider below
+// const SubscriptionContextProvider = (props) => {
+
+//   return (
+//     <SubscriptionContext.Provider value={/* pass praoper and attribute */}>
+//       {props.children}
+//     </SubscriptionContext.Provider>
+//   );
+// };
 export default SubscriptionContext;
 // {} because it is Filter Handler Function in App .js
 // IDE is notify by []
+
+// LIMITATION OF USING CONTEXT
+// 1. Context is not for a button
+// 2. Not optimized for frequently changing data in that case props used
+// 3. context is only use when you have data chaining.
